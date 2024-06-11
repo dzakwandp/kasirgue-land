9ae066ca47d6027b3d0c2affd3777da57757ed0a
Dzakwan Diego <diegodzakwan@gmail.com>
2024-06-11 08:12:50 +0700

    cleaning up

--- pages/index.vue
+++ pages/index.vue
@@ -131,56 +131,62 @@
         Dengan fitur-fitur diatas, tentunya Kasirgue akan berikan harga yang
         menarik buat Loe-Loe yang ingin mengembangkan usaha!
       </div>
-      <div class="flex w-full mt-10">
-        <div class="flex flex-col md:w-1/2">
-          <div class="flex md:flex-row flex-col justify-center md:items-stretch items-center gap-4 h-max">
-            <Card
-              class="flex md:w-1/3 w-2/3 flex-col gap-2 px-4 items-center shadow-none">
-              <div
-                class="flex w-2/3 bg-primary text-white text-xl p-2 rounded-b-sm justify-center">
-                Basic
-              </div>
-              <CardContent class="font-number text-center">
-                <div class="text-2xl text-primary font-bold">Rp5.000/hari</div>
-                <div>Rp150.000/bulan</div>
-                <div class="line-through mt-4 text-black/60">
-                  Rp1.800.000/tahun
+      <div class="flex flex-col w-full mt-10">
+        <div class="flex w-full mt-10">
+          <div class="flex flex-col md:w-1/2">
+            <div
+              class="flex md:flex-row flex-col justify-end md:items-stretch items-center gap-4 h-max">
+              <Card
+                class="flex md:w-1/3 w-2/3 flex-col gap-2 px-4 items-center shadow-none">
+                <div
+                  class="flex w-2/3 bg-primary text-white text-xl p-2 rounded-b-sm justify-center">
+                  Basic
                 </div>
-                <div class="text-xl text-primary font-bold">
-                  Rp1.500.000/tahun
+                <CardContent class="font-number text-center">
+                  <div class="text-2xl text-primary font-bold">
+                    Rp5.000/hari
+                  </div>
+                  <div>Rp150.000/bulan</div>
+                  <div class="line-through mt-4 text-black/60">
+                    Rp1.800.000/tahun
+                  </div>
+                  <div class="text-xl text-primary font-bold">
+                    Rp1.500.000/tahun
+                  </div>
+                  <div class="italic text-xs">
+                    *khusus langganan langsung selama satu tahun
+                  </div>
+                </CardContent>
+              </Card>
+              <Card
+                class="flex md:w-1/3 w-2/3 flex-col gap-2 items-center shadow-none">
+                <div
+                  class="flex w-2/3 bg-secondary text-white text-xl p-2 rounded-b-sm justify-center">
+                  Premium
                 </div>
-                <div class="italic text-xs">
-                  *khusus langganan langsung selama satu tahun
-                </div>
-              </CardContent>
-            </Card>
-            <Card class="flex md:w-1/3 w-2/3 flex-col gap-2 items-center shadow-none">
-              <div
-                class="flex w-2/3 bg-secondary text-white text-xl p-2 rounded-b-sm justify-center">
-                Premium
-              </div>
-              <CardContent
-                class="flex flex-col items-center text-center font-number">
-                Hubungi gue untuk tentuin fitur yang Loe butuhin
-                <a
-                  href="http://api.whatsapp.com/send?phone=6285888080045&text=Hallo%20kak,%20mau%20informasi%20selanjutnya%20tentang%20Kasirgue%20dong"
-                  target="_blank">
-                  <Button class="text-white size-max gap-2">
-                    <Whatsapp class="w-6 fill-white" />Chat Gue
-                  </Button>
-                </a>
-              </CardContent>
-            </Card>
+                <CardContent
+                  class="flex flex-col items-center text-center font-number gap-2">
+                  Hubungi gue untuk tentuin fitur yang Loe butuhin
+                  <a
+                    href="http://api.whatsapp.com/send?phone=6285888080045&text=Hallo%20kak,%20mau%20informasi%20selanjutnya%20tentang%20Kasirgue%20dong"
+                    target="_blank">
+                    <Button class="text-white size-max gap-2">
+                      <Whatsapp class="w-6 fill-white" />Chat Gue
+                    </Button>
+                  </a>
+                </CardContent>
+              </Card>
+            </div>
           </div>
-          <div class="text-white text-center mt-4">
-            *Harga diatas sudah termasuk free akses 45 hari pertama.
-          </div>
-          <div class="text-white text-center md:text-5xl text-4xl font-semibold">
-            #WaktunyaOnline
+          <div class="hidden md:flex w-1/2 justify-center">
+            <img :src="MascotDrink" class="w-1/5" />
           </div>
         </div>
-        <div class="hidden md:flex w-1/2 justify-center">
-          <img :src="MascotDrink" class="w-1/5" />
+        <div class="text-white text-center mt-4">
+          *Harga diatas sudah termasuk free akses 45 hari pertama.
+        </div>
+        <div class="text-white text-center md:text-5xl text-4xl font-semibold">
+          #WaktunyaOnline
         </div>
       </div>
     </div>
